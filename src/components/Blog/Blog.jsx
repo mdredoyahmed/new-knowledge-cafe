43695c0c50c3e273663e7a05e9e471f3e2e3326c
mdredoyahmed <redoy-15-1852@diu.edu.bd>
2025-09-09 02:33:51 +0600

    adding cover post date ,author etc

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,30 @@
 
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
+const Blog = ({ blog }) => {
+  const {title, cover,author_img,author,posted_date,reading_time,hashtags} = blog;
     return (
         <div>
-            <h1></h1>
+            <img src={cover} alt= {`Cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                 <div className='flex'>
+                   <img className='w-14'  src={author_img} alt="" />
+                   <div className='ml-6'>
+                    <h3 className='text-2xl'>{author}</h3>
+                    <p>{posted_date}</p>
+
+                   </div>
+                 </div>
+                 <div>
+                    <span>{reading_time} min read</span>
+                    <button></button>
+                 </div>
+            </div>
+            <h1 className='text-4xl'>{title}</h1>
+            <p>
+                {
+                    hashtags.map((hash ,idx)=> <span key={idx}> <a> #{hash}</a> </span>)
+                }
+            </p>
         </div>
     );
 };
